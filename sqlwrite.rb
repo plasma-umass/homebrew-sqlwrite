f85665cdfc6cc8bcd0971ad8aace0458e7daaf3b
Emery Berger <emery.berger@gmail.com>
2024-10-25 09:52:59 -0400

    Initial.

---
+++ sqlwrite.rb
@@ -0,0 +1,20 @@
+class Sqlwrite < Formula
+  desc "Command-line tool for SQL tasks"
+  homepage "https://github.com/plasma-umass/sqlwrite"
+  url "https://github.com/plasma-umass/sqlwrite.git", branch: "main"
+  license "Apache-2.0"
+
+  depends_on "make" # Add other dependencies if needed
+
+  def install
+    # Clone and build using make
+    system "make"
+    # Install the binaries, assuming 'sqlwrite' is the main executable
+    bin.install "sqlwrite"
+  end
+
+  test do
+    # Test the installation
+    system "#{bin}/sqlwrite", "--version"
+  end
+end
